50666d8ff2a7058754b7b82395575af633c9f49b
Cherry2403 <155967650+Cherry2403@users.noreply.github.com>
2024-06-06 04:07:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let boardMain;
 let boardChar;
-/*let boardMain_width;
+let boardMain_width;
 let boardMain_height;
 let boardChar_width;
 let boardChar_height;
 
-let charHeight = 50;
+/*let charHeight = 50;
 let charWidth= 40;
 let charX= board_width/2;
 let charY= board_height/2 + 30;
@@ -31,20 +31,62 @@ function boardSizes()
     boardMain= document.getElementById('boardMain');
     boardChar= document.getElementById('boardChar');
 
-    /*boardMain_width= boardMain.width;
-    boardMain_height= boardMain.height;*/
-
-    if(window.innerWidth >=600)
+    if(window.innerWidth >=1000)
     {
-        boardChar.width=150;
-        boardChar.height=450;
+        boardChar.width=100;
+        boardChar.height=550;
+        boardMain.width=650;
+        boardMain.height=550;
+        /*boardMain.style.marginRight= (window.innerWidth - 300)/2 + 'px';*/
+        
+        boardMain.style.marginRight= 280+'px';
+        boardMain.style.marginLeft = auto;
+        boardChar.style.marginLeft = window.innerWidth/15+'px';
     }
-    else
+
+    if(window.innerWidth >=900 && window.innerWidth <=999)
+    {
+        boardChar.width=100;
+        boardChar.height=550;
+        boardMain.width=600;
+        boardMain.height=550;
+        /*boardMain.style.marginRight= (window.innerWidth - 300)/2 + 'px';*/
+        boardMain.style.marginRight = 200+'px';
+        boardMain.style.marginLeft = auto;
+        boardChar.style.marginLeft = window.innerWidth/15+'px';
+    }
+    else if(window.innerWidth >=700 &&window.innerWidth <=899)
+    {
+        boardChar.width=100;
+        boardChar.height=550;
+        boardMain.width=550;
+        boardMain.height=550;
+        boardMain.style.marginLeft = 'auto';
+        boardMain.style.marginRight = 'auto';
+        boardChar.style.marginLeft = 'auto';
+        boardChar.style.marginRight = 'auto';
+
+        
+        
+    }
+    else if(window.innerWidth <699)
     {
         boardChar.height=100;
-        boardChar.width=450;
+        boardChar.width=550;
+        boardMain.width=550;
+        boardMain.height=550;
+        boardMain.style.marginLeft = 'auto';
+        boardMain.style.marginRight = 'auto';
+        boardChar.style.marginLeft = 'auto';
+        boardChar.style.marginRight = 'auto';
     }
+    
+    
 }
 
 window.addEventListener('resize', boardSizes);
 document.addEventListener('DOMContentLoaded', boardSizes);
+boardChar_width=boardChar.width;
+boardChar_height= boardChar.height;
+boardMain_width= boardMain.width;
+boardMain_height= boardMain.height;
