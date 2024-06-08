71637f1132b1f3f8acded7ed2ab261653bece7cd
Cherry2403 <155967650+Cherry2403@users.noreply.github.com>
2024-06-08 18:15:56 +0200

    mainBoardsetup

--- script.js
+++ script.js
@@ -1,92 +1,171 @@
-let boardMain;
-let boardChar;
-let boardMain_width;
-let boardMain_height;
-let boardChar_width;
-let boardChar_height;
+var boardMain;
+var boardChar;
+var boardMain_width;
+var boardMain_height;
+var boardChar_width;
+var boardChar_height;
 
-/*let charHeight = 50;
-let charWidth= 40;
-let charX= board_width/2;
-let charY= board_height/2 + 30;
+//value for x,y coor of mainBoard
+var context;
+var topLeft;
+var topRight;
+var bottomLeft;
+var bottomRight;
+var rect1;
 
-let char={
-    img:null,
-    x:charX,
-    y:charY,
-    width: charWidth,
-    height: charHeight
+//for char
+var normalImg;
+
+var char = {
+    img: null,
+    x: 100,
+    y: 100,
+    width: 80,
+    height: 0
 }
 
-/*window.onload=function()
-{
+//for platform
+var platformArray = [];
+var platformImg = ['./art/intro.png', './art/skills.png', './art/experience.png', './art/education.png', './art/coursework.png'];
+var platDist = 110;
+var imagesLoaded = 0;
+
+function setUp() {
     boardSizes();
-    window.addEventListener('resize', boardSizes);
-}*/
-
-
-
-function boardSizes()
-{
-    boardMain= document.getElementById('boardMain');
-    boardChar= document.getElementById('boardChar');
-
-    if(window.innerWidth >=1000)
-    {
-        boardChar.width=100;
-        boardChar.height=550;
-        boardMain.width=650;
-        boardMain.height=550;
-        /*boardMain.style.marginRight= (window.innerWidth - 300)/2 + 'px';*/
-        
-        boardMain.style.marginRight= 280+'px';
-        boardMain.style.marginLeft = auto;
-        boardChar.style.marginLeft = window.innerWidth/15+'px';
-    }
-
-    if(window.innerWidth >=900 && window.innerWidth <=999)
-    {
-        boardChar.width=100;
-        boardChar.height=550;
-        boardMain.width=600;
-        boardMain.height=550;
-        /*boardMain.style.marginRight= (window.innerWidth - 300)/2 + 'px';*/
-        boardMain.style.marginRight = 200+'px';
-        boardMain.style.marginLeft = auto;
-        boardChar.style.marginLeft = window.innerWidth/15+'px';
-    }
-    else if(window.innerWidth >=700 &&window.innerWidth <=899)
-    {
-        boardChar.width=100;
-        boardChar.height=550;
-        boardMain.width=550;
-        boardMain.height=550;
-        boardMain.style.marginLeft = 'auto';
-        boardMain.style.marginRight = 'auto';
-        boardChar.style.marginLeft = 'auto';
-        boardChar.style.marginRight = 'auto';
-
-        
-        
-    }
-    else if(window.innerWidth <699)
-    {
-        boardChar.height=100;
-        boardChar.width=550;
-        boardMain.width=550;
-        boardMain.height=550;
-        boardMain.style.marginLeft = 'auto';
-        boardMain.style.marginRight = 'auto';
-        boardChar.style.marginLeft = 'auto';
-        boardChar.style.marginRight = 'auto';
-    }
-    
-    
+    charSetUp();
+    platformSetUp();
 }
 
-window.addEventListener('resize', boardSizes);
-document.addEventListener('DOMContentLoaded', boardSizes);
-boardChar_width=boardChar.width;
-boardChar_height= boardChar.height;
-boardMain_width= boardMain.width;
-boardMain_height= boardMain.height;
+function boardSizes() {
+    boardMain = document.getElementById('boardMain');
+    boardChar = document.getElementById('boardChar');
+
+    if (window.innerWidth >= 850) {
+        boardChar.width = 100;
+        boardChar.height = 650;
+        boardMain.width = 650;
+        boardMain.height = 650;
+        boardMain.style.marginRight = 280 + 'px';
+        boardMain.style.marginLeft = 'auto';
+        boardChar.style.marginLeft = window.innerWidth / 15 + 'px';
+    } else if (window.innerWidth < 849) {
+        boardChar.height = 100;
+        boardChar.width = 550;
+        boardMain.width = 550;
+        boardMain.height = 650;
+        boardMain.style.marginLeft = 'auto';
+        boardMain.style.marginRight = 'auto';
+        boardChar.style.marginLeft = 'auto';
+        boardChar.style.marginRight = 'auto';
+    }
+
+    context = boardMain.getContext('2d');
+
+    boardChar_width = boardChar.width;
+    boardChar_height = boardChar.height;
+    boardMain_width = boardMain.width;
+    boardMain_height = boardMain.height;
+}
+
+//find x,y of 4 corners, place char
+function charSetUp() {
+    rect1 = boardMain.getBoundingClientRect();
+    topLeft = { x: rect1.left + window.scrollX, y: rect1.top + window.scrollY };
+    topRight = { x: rect1.right + window.scrollX, y: rect1.top + window.scrollY };
+    bottomLeft = { x: rect1.left + window.scrollX, y: rect1.bottom + window.scrollY };
+    bottomRight = { x: rect1.right + window.scrollX, y: rect1.bottom + window.scrollY };
+
+    normalImg = new Image();
+    normalImg.src = "./art/bia/jump.png";
+    char.img = normalImg;
+    char.x = (topRight.x - topLeft.x) / 2 - char.width / 2;
+    char.y = (bottomLeft.y - topLeft.y) / 2 - char.height / 2 + 150;
+    if (window.innerWidth < 540) {
+        char.x = window.innerWidth / 2 - char.width / 2;
+    }
+    
+    normalImg.onload = function () {
+        char.height = (normalImg.naturalHeight / normalImg.naturalWidth) * char.width;
+        imagesLoaded++;
+        // Check if all images are loaded
+        checkAllImagesLoaded();
+    }
+}
+
+function platformSetUp() {
+    platformArray = [];
+    imagesLoaded = 1; // Reset imagesLoaded to account for the character image
+
+    for (let i = 0; i < platformImg.length; i++) {
+        let platformImgElement = new Image();
+        platformImgElement.src = platformImg[i];
+        
+        let platform = {
+            img: platformImgElement,
+            x: 0,
+            y: 0,
+            width: 130,
+            height: 30
+        };
+        
+        platformImgElement.onload = function () {
+            imagesLoaded++;
+            // Check if all images are loaded
+            checkAllImagesLoaded();
+        }
+        
+        platformArray.push(platform);
+    }
+
+    platformArray[0].x = char.x;
+    platformArray[0].y =  580;
+    platformArray[0].width = 100;
+    platformArray[0].height = 30;
+
+    for (let i = 1; i < platformArray.length; i++) {
+        if (i % 2 == 0) {
+            platformArray[i].x = 50;
+            platformArray[i].y = 650 - i * platDist;
+        } else {
+            platformArray[i].x = 380;
+            platformArray[i].y = 450 - i * platDist;
+        }
+
+        if (boardMain.width > 560 && i % 2 == 1) {
+            platformArray[i].x = 470;
+        }
+
+        if (window.innerWidth < 550) {
+            // Adjust positions for smaller screens if necessary
+            platformArray[i].width=75;
+            platformArray[i].height = 23;
+            if (i%2==1)
+            {
+                platformArray[i].x = window.innerWidth*2/3;
+            }
+        }
+    }
+}
+
+function checkAllImagesLoaded() {
+    if (imagesLoaded === platformImg.length + 1) { // +1 for the character image
+        drawAllImages();
+    }
+}
+
+function drawAllImages() {
+    context.clearRect(0, 0, boardMain.width, boardMain.height);
+
+    // Draw character
+    context.drawImage(char.img, char.x, char.y, char.width, char.height);
+
+    // Draw platforms
+    for (let i = 0; i < platformArray.length; i++) {
+        context.drawImage(platformArray[i].img, platformArray[i].x, platformArray[i].y, platformArray[i].width, platformArray[i].height);
+    }
+}
+
+//reload everytime got change in window size
+window.addEventListener('resize', setUp);
+document.addEventListener('DOMContentLoaded', setUp);
+
